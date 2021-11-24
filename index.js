3ea7a9431c68dfbc0eefe6faa93270d8129b3ca9
Laxuzer <64734729+Laxuzer@users.noreply.github.com>
2021-11-24 19:30:42 +0300

    logger :|

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const Handler = new (require('./src/handlers/handler'))(client);
 Handler.init();
 
 client.login(client.config.token)
-    .then(() => client.logger.templates.Log('Discord', 'Bot Aktif!'))
+    .then(() => client.logger.log('Discord', 'Bot Aktif!'))
     .catch((e) => client.ErrorHandler.error({ errorCode: 'ClientLogin', error: e.message }));
