cc92012d64a275338f1548c6f1105a74113b1c4e
Laxuzer <64734729+Laxuzer@users.noreply.github.com>
2021-11-24 19:06:04 +0300

    hata

--- src/handlers/auto/eventHandler.js
+++ src/handlers/auto/eventHandler.js
@@ -9,7 +9,7 @@ module.exports = class CmdHandler extends Handler {
             enabled: true,
             run: async (client) => {
                 const events = await readdirSync('./src/events');
-                client.logger.templates.Log('EventHandler', `Loading a total of ${events.length} events.`, { TitleColor: 'green' });
+                client.logger.log('EventHandler', `Loading a total of ${events.length} events.`, { TitleColor: 'green' });
                 client.removeAllListeners();
                 for (const event of events) {
                     const checkCommandLoaded = require.cache[require.resolve(`../../events/${event}`)];
@@ -18,7 +18,7 @@ module.exports = class CmdHandler extends Handler {
                     const e = new Event(client);
                     if (!e.enabled) return;
                     client.on(e.name, async (...args) => await e.run(...args));
-                    client.logger.templates.Log('EventHandler', `Loading Event: ${e.name}. 👌`, { TitleColor: 'green' });
+                    client.logger.log('EventHandler', `Loading Event: ${e.name}. 👌`, { TitleColor: 'green' });
                     delete require.cache[require.resolve(`../../events/${event}`)];
                 }
             }
