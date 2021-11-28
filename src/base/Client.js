4bffda093eb804d05a030341bc993f02bd6af4a0
Laxuzer <64734729+Laxuzer@users.noreply.github.com>
2021-11-28 14:30:45 +0300

    slash handler
    slash handleri unutmşm

--- src/base/Client.js
+++ src/base/Client.js
@@ -3,6 +3,7 @@ const { Client, Intents } = require("discord.js");
 module.exports = class SuperBot extends Client {
     constructor() {
         const CommandHandler = require('./CommandHandler')
+        const SlashHandler = require('./SlashHandler')
         const Logger = require('./Logger')
         const Emoji = require('./Emoji')
         const ErrorHandler = require('./ErrorHandler')
@@ -10,13 +11,13 @@ module.exports = class SuperBot extends Client {
         const FileSync = require('lowdb/adapters/FileSync');
         const adapter = new FileSync('./src/database/db.json');
         const db = low(adapter);
-        global.db = db;
         super({
             intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
             restTimeOffset: 100,
             messageCacheMaxSize: 100,
         })
         this.CommandHandler = new CommandHandler(this)
+        this.SlashHandler = new SlashHandler(this)
         this.ErrorHandler = new ErrorHandler()
         this.logger = new Logger()
         this.Emoji = new Emoji(this)
