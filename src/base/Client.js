18ab38aa6c0e2110f6113775a703cb801b4441e6
Laxuzer <64734729+Laxuzer@users.noreply.github.com>
2022-02-25 20:30:46 +0300

    Update Client.js

--- src/base/Client.js
+++ src/base/Client.js
@@ -7,12 +7,12 @@ module.exports = class SuperBot extends Client {
         const Logger = require('./Logger')
         const Emoji = require('./Emoji')
         const ErrorHandler = require('./ErrorHandler')
-        const low = require('lowdb'); 
-        const FileSync = require('lowdb/adapters/FileSync');
-        const adapter = new FileSync('./src/database/db.json');
-        const db = low(adapter);
+        const { JsonDatabase } = require('wio.db');
+        const db = new JsonDatabase({ databasePath: './src/database/db.json' });
+        global.db = db;
         super({
-            intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+            intents: ['GUILDS', 'GUILD_MESSAGES', 'DIRECT_MESSAGES', 'GUILD_MEMBERS'],
+            partials: ['CHANNEL', 'GUILD_MEMBER', 'MESSAGE', 'REACTION', 'USER'],
             restTimeOffset: 100,
             messageCacheMaxSize: 100,
         })
